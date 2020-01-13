bf02d7545162777c3d1294ced1aa676b7eb7ff97
HKFPouu <389746410@qq.com>
2020-01-13 20:39:59 +0800

    fixed: 身份判定更改

--- Reservation-System/pages/cognitiveLearning/cognitiveLearning.js
+++ Reservation-System/pages/cognitiveLearning/cognitiveLearning.js
@@ -1,4 +1,3 @@
-const app = getApp();
 const util = require("../../utils/util");
 
 Page({
--- Reservation-System/pages/index/index.js
+++ Reservation-System/pages/index/index.js
@@ -25,7 +25,7 @@ Page({
       pageId: pageId
     })
 
-    if (this.data.role == 1) { // 更改身份 0普通 1管理
+    if (this.data.role == 0) { // 更改身份 0普通 1管理
       let page = this.data.pageId == 1 ? 'teamVisit' : 'cognitiveLearning'
       wx.navigateTo({
         url: `../${page}/${page}`
